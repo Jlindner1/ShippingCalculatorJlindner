98cab07d6415d9bac59db1c14dae66dec0140acd
Jlindner1 <46451910+Jlindner1@users.noreply.github.com>
2020-09-18 09:47:35 -0600

    in class practice of querySelectors and variables

--- js/index.js
+++ js/index.js
@@ -1,31 +1,61 @@
-  /* 
- select the input element
- store a reference to the input element in a variable
- output the value  of your variable to the console.
- set a value for the shipping rate per 1kg store it in a variable
+/* 
+getElementById(id)
+return value: element or group of elements markup
+const shippingweight = document.getElementById('weight')
+console.log(shippingweight)
+*/
+
+/* 
+getElementByClassName(css class name)
+return type is HTMLCollect
+** class name does not have dot **
+const testThings = document.getElementsByClassName('test')
+console.log(testThings)
+*/
+
+/* 
+getElementByTagName(element)
+const ptag = document.getElementsByTagName('li')
+console.log(ptag)
+*/
+
+/* 
+querySelector(element)
+*/
+
+const inputField = document.querySelector('input');
+console.log(inputField);
+
+/* 
+Read Data from the dom using a button click as the event that drives the code 
+*/
+
+//reference variables
+
+const calculate = document.querySelector('label');
+const shippingWeight = document.querySelector('input');
+const shippingRate = 5;
+const shippingPrice = document.querySelector('.shipping-price');
+shippingPrice.textContent = "$0.00";
 
 
-      object     .    method()
-      object    . property()
-      object   . event()
 
-    document.querySelector('css')
-
-    let =    allows you to overite the value
-    const = once only   Pie = 3.12
- 
- */
-
-//  reference variable
-// variable name   =   statement
-
- 
-
-const thing = document.querySelector('blockquote')
-console.log(thing)
- 
- 
- 
+calculate.addEventListener('click', function(){
+  const rate = Number(shippingWeight.value) * shippingRate;
+  
+  shippingPrice.textContent = "$"+rate+".00";
+  shippingPrice.style.fontSize = "4rem";
+  
+});
 
 
- 
+
+// const loginButton = document.getElementById('login')
+// loginButton.addEventListener('click', function(){
+//   console.log('login clicked');
+// });
+
+// const imageLogo = document.querySelector('img')
+// imageLogo.addEventListener('mouseenter', function(){
+//   console.log('logo hover');
+// });
